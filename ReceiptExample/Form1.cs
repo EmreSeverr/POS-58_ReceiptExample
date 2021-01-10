d7ab871c8ad8d3d1770a883821da2efa27796a2a
Emre Sever <emresever@milvasoft.com>
2021-01-11 02:21:56 +0300

    uninstall package.

--- ReceiptExample/Form1.cs
+++ ReceiptExample/Form1.cs
@@ -1,16 +1,7 @@
-﻿using PrinterUtility;
-using System;
-using System.Collections;
+﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Drawing.Imaging;
 using System.Drawing.Printing;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ReceiptExample
